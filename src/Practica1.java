04556abaf2f5ddaa7220d7ea09c8fd60f8b9374f
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-09-28 10:27:15 +0100

    // Añadir Nombres y notas con while  // Genera numero random que por cada vuelta del bucle cambiara para obtener las 4 notas en el rango del 0 al 10

--- src/Practica1.java
+++ src/Practica1.java
@@ -1,4 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+
 public class Practica1 {
 
+    public static void Mostrar(String archivo) throws FileNotFoundException, IOException {
+
+        // Array de Nombres y Notas
+        ArrayList<String> Nombres = new ArrayList<>();
+        ArrayList<Double> Notas = new ArrayList<>();
+        FileReader f = new FileReader(archivo);
+        BufferedReader reader = new BufferedReader(f);
+
+        // Añadir Nombres y notas con while
+        while ((reader.readLine()) != null) {
+            Nombres.add((reader.readLine()));
+            // Genera numero random que por cada vuelta del bucle cambiara para obtener las 4 notas en el rango del 0 al 10
+            for (int e = 0; e < 4; e++) {
+                Random rand = new Random();
+                Double n = (double) rand.nextInt(10);
+                Notas.add(n);
+            }
+        }
+
+
+}
 
 }
