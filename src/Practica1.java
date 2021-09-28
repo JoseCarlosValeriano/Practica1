d5915208d7f2b6c23c7a05c6002f52c7f55a0e16
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-09-28 10:29:14 +0100

    // Se instancia el fichero y el escritor de cuyo archivo volcaremos el resultado      //Imprimimos con un numero random los nombres aleatorios con sus notas aleatorias

--- src/Practica1.java
+++ src/Practica1.java
@@ -27,7 +27,38 @@ public class Practica1 {
             }
         }
 
+        reader.close();
+        // Se instancia el fichero y el escritor de cuyo archivo volcaremos el resultado
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        fichero = new FileWriter("/Users/josecarlos/Documents/FP Grado Superior/2º Curso/Programación multimedia y dispositivos móviles - Goyo/1º Tema/Practica1-1Hecha.txt");
+        pw = new PrintWriter(fichero);
+        //Imprimimos con un numero random los nombres aleatorios con sus notas aleatorias
+        for (int i = 0; i < 10; i++) {
+            Random rand2 = new Random();
+            int n2 = rand2.nextInt(39);
+
+            System.out.print("\n" + Nombres.get(n2));
+            System.out.print("    | " + Notas.get(i));
+            System.out.print("    | " + Notas.get(i + 1));
+            System.out.print("    | " + Notas.get(i + 2));
+            System.out.print("    | " + Notas.get(i + 3));
+            System.out.print("|     La media es: " + ((Notas.get(i) + Notas.get(i + 2) + Notas.get(i + 3))) / 4);
+
+            pw.print("\n" + Nombres.get(n2));
+            pw.print("    | " + Notas.get(i));
+            pw.print("    | " + Notas.get(i + 1));
+            pw.print("    | " + Notas.get(i + 2));
+            pw.print("    | " + Notas.get(i + 3));
+            pw.print("|     La media es: " + ((Notas.get(i) + Notas.get(i + 2) + Notas.get(i + 3))) / 4);
+        }
+        pw.close();
+        System.out.println("Se ha creado el archivo: " + fichero);
+    }
+
 
 }
+
+
 
 }
